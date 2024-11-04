fab6b8609f727eae7cb29e0900bc7e309f57fca1
kactrulya <greenfrost5@yandex.ru>
2024-11-04 18:32:58 +0300

    junit

--- src/main/java/org/example/FactorialCalculatorTest.java
+++ src/main/java/org/example/FactorialCalculatorTest.java
@@ -1,28 +1,29 @@
 package org.example;
 
+import org.junit.jupiter.api.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 import org.testng.annotations.AfterMethod;
 
 public class FactorialCalculatorTest {
 
 
-    @BeforeMethod
-    public void before(){
+    @BeforeAll
+    public static void someInfo(){
         System.out.println("этот метод выполнился до факториала");
     }
 
-    @Test
+    @RepeatedTest(3)
+    @DisplayName("факториал")
     public void testCalculateFactorial() {
         FactorialCalculator calculator = new FactorialCalculator();
 
-        Assert.assertEquals(1, calculator.calculateFactorial(0));
-        Assert.assertEquals(1, calculator.calculateFactorial(1));
-        Assert.assertEquals(2, calculator.calculateFactorial(2));
-        Assert.assertEquals(6, calculator.calculateFactorial(3));
-        Assert.assertEquals(120, calculator.calculateFactorial(5));
+        Assertions.assertEquals(1, calculator.calculateFactorial(0));
+        Assertions.assertEquals(1, calculator.calculateFactorial(1));
+        Assertions.assertEquals(2, calculator.calculateFactorial(2));
+        Assertions.assertEquals(6, calculator.calculateFactorial(3));
+        Assertions.assertEquals(120, calculator.calculateFactorial(5));
     }
     @AfterMethod
     public void after() {
